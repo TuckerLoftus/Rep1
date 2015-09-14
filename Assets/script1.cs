67b2da0b850dabfd2786163a5f5faa391d458921
TuckerLoftus <tuckerloftus97@gmail.com>
2015-09-14 14:14:05 -0400

    added Assets and ProjectSettings for CubeDays

---
+++ Assets/script1.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class script1 : MonoBehaviour {
+
+	bool CubeSpawned = false;
+
+	public GameObject cube;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void SpawnCube () {
+		Instantiate (cube, new Vector3(0,0,0), Quaternion.identity); 
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > 3 && CubeSpawned == false) {
+			SpawnCube ();
+			CubeSpawned = true;
+		}
+	
+	}
+}
